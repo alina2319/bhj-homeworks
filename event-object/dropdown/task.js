fa22349b8a80d27eaf5699f267f39c3d6214d21c
Alina <Alin.k4@yandex.ru>
2022-11-20 21:57:29 +0300

    first commit ads

--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -1,22 +1,17 @@
-const dropdownValue = document.querySelector(".dropdown__value");
-const dropdownList = document.querySelector(".dropdown__list");
-dropdownValue.onclick = function () {
-    if (dropdownList.classList == 'dropdown__list dropdown__list_active') {
-        dropdownList.classList.remove('dropdown__list_active');
-    } else {
-        dropdownList.classList.add('dropdown__list_active');
-    }
+const dropdowns = [...document.querySelectorAll('.dropdown__value')];
+const dropdownLists = [...document.querySelectorAll('.dropdown__list')];
+
+dropdowns.forEach((el, index) => el.addEventListener('click', () => onDropdownClick(index)));
+dropdownLists.forEach((el, index) => el.addEventListener('click', (e) => onDropdownListClick(e, index)));
+
+function onDropdownClick(i) {
+    dropdownLists[i].classList.toggle('dropdown__list_active');
 }
-const dropdownItem = document.body.querySelectorAll(".dropdown__item");
-for (let i = 0; i < dropdownItem.length; i++) {
-    dropdownItem[i].addEventListener("click", function (event) {
-        const er = this.closest('.dropdown__list');
-        if (er.classList[0] == 'dropdown__list') {
-            event.target.onclick = function () {
-                return false;
-            }
-            dropdownValue.textContent = event.target.textContent;
-            dropdownList.classList.remove('dropdown__list_active');
-        }
-    });
+
+function onDropdownListClick(e, i) {
+    e.preventDefault();
+    if (e.target.classList.contains('dropdown__link')) {
+        dropdowns[i].textContent = e.target.textContent;
+    }
+    dropdownLists[i].classList.remove('dropdown__list_active');
 }
