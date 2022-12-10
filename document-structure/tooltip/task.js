c67d65c3178488811f814cbd0872c78683b70e96
Alina <Alin.k4@yandex.ru>
2022-12-10 21:12:57 +0300

    first commit ads

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,15 +1,44 @@
-const links = document.querySelectorAll(".has-tooltip");
+"use strict";
 
-for (let i = 0; i < links.length; i++) {
-    let placeLinks = links[i].getBoundingClientRect();
-    let hints = `<div class="tooltip tooltip_active" style="left:${placeLinks.x}px">${links[i].title}</div>`;
+const hasTooltip = document.querySelectorAll(`.has-tooltip`);
+const hasTooltipLength = hasTooltip.length;
 
-    links[i].addEventListener("click", (event) => {
-        event.preventDefault();
-        if(!event.target.nextElementSibling.classList.contains("tooltip_active")) {
-            event.target.insertAdjacentHTML("afterEnd", hints)
-        } else {
-            event.target.nextElementSibling.remove();
-        }
-    });
+let tooltipList;
+
+function tooltipsMaker() {
+    for (let i = 0; i < hasTooltipLength; i++) {
+        hasTooltip[i].insertAdjacentHTML(`afterEnd`, `<div class="tooltip">${hasTooltip[i].title}</div>`);
+    }
+
+    tooltipList = document.querySelectorAll(`.tooltip`);
+
+    for (let i = 0; i < hasTooltipLength; i++) {
+        hasTooltip[i].addEventListener(`click`, function() {
+            event.preventDefault();
+
+            if ( tooltipList[i].classList.contains(`tooltip_active`) ) {
+                tooltipList[i].classList.remove(`tooltip_active`);
+            } else {
+                tooltipListRemover();
+
+                const leftIndent = hasTooltip[i].getBoundingClientRect().left;
+                const topIndent = hasTooltip[i].getBoundingClientRect().top;
+                const bottomIndent = hasTooltip[i].getBoundingClientRect().bottom;
+
+                tooltipList[i].style = `left: ${leftIndent}px; top: ${topIndent + (bottomIndent - topIndent)}px`;
+
+                tooltipList[i].classList.add(`tooltip_active`);
+            }
+
+            document.addEventListener(`scroll`, tooltipListRemover);
+        });
+    }
 }
+
+function tooltipListRemover() {
+    for (let i = 0; i < tooltipList.length; i++) {
+        tooltipList[i].classList.remove(`tooltip_active`);
+    }
+}
+
+document.addEventListener(`DOMContentLoaded`, tooltipsMaker);
